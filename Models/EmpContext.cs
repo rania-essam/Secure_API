fd1852694ef088cd1a79dc7badbe05662179635b
RaniaEssam <ranon6389@gmail.com>
2026-05-21 16:38:26 +0300

    Add files via upload

---
+++ Models/EmpContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI_DAY3.Models
+{
+    public class EmpContext : IdentityDbContext
+    {
+        public EmpContext(DbContextOptions<EmpContext> options):base(options)
+        {
+            
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+
+            builder.Entity<IdentityRole>().HasData(
+
+                new IdentityRole() { Id = "1", Name = "employee", NormalizedName = "EMPLOYEE", ConcurrencyStamp = "1" },
+                new IdentityRole() { Id = "2", Name = "manager", NormalizedName = "MANAGER", ConcurrencyStamp = "2" }
+                );
+
+
+            base.OnModelCreating(builder);
+
+
+        }
+
+        public DbSet<Employee> Employess { get; set; }
+
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI_DAY3.Models
+{
+    public class Employee : IdentityUser
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public decimal Age { get; set; }
+    }
+}
