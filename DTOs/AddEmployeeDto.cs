a7c04466edd2e52d42fb82c2b6c92c16fc0ee424
RaniaEssam <ranon6389@gmail.com>
2026-05-21 16:37:31 +0300

    Add files via upload

---
+++ DTOs/AddEmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPI_DAY3.DTOs.AccountDtos
+{
+    public class AddEmployeeDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public decimal age { get; set; }
+
+    }
+}
